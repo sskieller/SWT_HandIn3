0d9fcdded01e960b7d1e68531311d59c6c213148
sskieller <soren.skieller@gmail.com>
2018-04-09 15:27:34 +0200

    added UTs for TransponderDataParser

--- TestUnit/UT_TransponderDataParser.cs
+++ TestUnit/UT_TransponderDataParser.cs
@@ -43,13 +43,101 @@ namespace TestUnit
             Assert.That(() => _uut.ParseTime(time), Throws.Exception.TypeOf<FormatException>());
         }
 
-        //ATR423;39045;12932;14000;20151006213456789
-        //[Test]
-        //public void ParseData_Test()
-        //{
-        //    var data = "ATR423;39045;12932;14000;20151006213456789";
-        //    _uut.ParseData(data);
-        //    Assert.That();
-        //}
+        [Test]
+        public void ParseData_MissingData_ExpectedResult_Exception()
+        {
+            string data = "A3";
+            Assert.That(() => _uut.ParseData(data,
+                out string tag,
+                out int xCoord,
+                out int yCoord,
+                out uint altitude,
+                out DateTime time), Throws.Exception.TypeOf<IndexOutOfRangeException>());
+        }
+
+        [TestCase("A3;39045;12932;14000;20151006213456789","A3",TestName = "Shorter Name")]
+        [TestCase("ATR423;39045;12932;14000;20151006213456789", "ATR423", TestName = "Legal input")]
+        public void ParseData_TagTest_ExpectedResult_True(string data, string expectedTag)
+        {
+            _uut.ParseData(data, 
+                out string tag, 
+                out int xCoord, 
+                out int yCoord, 
+                out uint altitude, 
+                out DateTime time);
+            
+            Assert.That(tag, Is.EqualTo(expectedTag));
+        }
+
+        [TestCase("ATR423;-23;12932;14000;20151006213456789", -23, TestName = "xCoord_-23")]
+        [TestCase("ATR423;0;12932;14000;20151006213456789",0, TestName = "xCoord_0")]
+        [TestCase("ATR423;39045;12932;14000;20151006213456789",39045, TestName = "xCoord_39045")]
+        public void ParseData_xCoordTest_ExpectedResult_True(string data, int xCor)
+        {
+            _uut.ParseData(data,
+                out string tag,
+                out int xCoord,
+                out int yCoord,
+                out uint altitude,
+                out DateTime time);
+
+            Assert.That(xCoord, Is.EqualTo(xCor));
+        }
+
+        [TestCase("ATR423;39045;-23;14000;20151006213456789", -23, TestName = "yCoord_-23")]
+        [TestCase("ATR423;39045;0;14000;20151006213456789",0,TestName = "yCoord_0")]
+        [TestCase("ATR423;39045;12932;14000;20151006213456789",12932,TestName = "yCoord_12932")]
+        public void ParseData_yCoordTest_ExpectedResult_True(string data, int yCor)
+        {
+            _uut.ParseData(data,
+                out string tag,
+                out int xCoord,
+                out int yCoord,
+                out uint altitude,
+                out DateTime time);
+
+            Assert.That(yCoord, Is.EqualTo(yCor));
+        }
+
+        [TestCase("ATR423;39045;12932;0;20151006213456789",0,TestName = "Altitude_0")]
+        [TestCase("ATR423;39045;12932;14000;20151006213456789",14000,TestName = "Altitude_14k")]
+        public void ParseData_AltitudeTestPositiveNumber_ExpectedResult_True(string data, int alt)
+        {
+            _uut.ParseData(data,
+                out string tag,
+                out int xCoord,
+                out int yCoord,
+                out uint altitude,
+                out DateTime time);
+
+            Assert.That(altitude, Is.EqualTo(alt));
+        }
+
+        [Test]
+        public void ParseData_AltitudeTestNegativeNumber_ExpectedResult_Exception()
+        {
+            string data = "ATR423;39045;12932;-32;20151006213456789";
+            Assert.That(()=> _uut.ParseData(data,
+                out string tag,
+                out int xCoord,
+                out int yCoord,
+                out uint altitude,
+                out DateTime time), Throws.Exception.TypeOf<OverflowException>());
+        }
+
+        [Test]
+        public void ParseData_ParseTimeTest_ExpectedResult_True()
+        {
+            string data = "ATR423;39045;12932;14000;20151006213456789";
+            var date = DateTime.Parse("2015-10-06 21:34:56.789");
+            _uut.ParseData(data,
+                out string tag,
+                out int xCoord,
+                out int yCoord,
+                out uint altitude,
+                out DateTime time);
+
+            Assert.That(time, Is.EqualTo(date));
+        }
     }
 }
